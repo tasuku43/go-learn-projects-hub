620fa3179b8d589cfe56e9fb88722ddf0c7abfd0
tasuku43 <task.teckac@gmail.com>
2024-01-11 23:59:31 +0900

    update: 環境変数によってロードバランシング方式を切り替えれるようにした

--- reverse-proxy/main.go
+++ reverse-proxy/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/joho/godotenv"
 	"github.com/tasuku43/go-learn-projects-hub/waf/pkg/load_balancer/least_connections"
+	"github.com/tasuku43/go-learn-projects-hub/waf/pkg/load_balancer/round_robin"
 	"github.com/tasuku43/go-learn-projects-hub/waf/pkg/middleware"
 	"log/slog"
 	"net/http"
@@ -19,19 +20,32 @@ func main() {
 		return
 	}
 
-	chainedHandler := middleware.Chain(
-		//round_robin.NewLoadBalancerHandler(logger, getBackendUrls()),
-		least_connections.NewLoadBalancerHandler(logger, getBackendUrls()),
-		middleware.NewMiddlewares(logger)...,
-	)
-
-	http.Handle("/", chainedHandler)
+	lbType := strings.ToLower(os.Getenv("LOAD_BALANCER_TYPE"))
+	http.Handle("/", handler(logger, lbType))
 
 	port := os.Getenv("PROXY_PORT")
 	logger.Info("Starting server on port: " + port)
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		return
 	}
+}
+
+func handler(logger *slog.Logger, lbType string) http.Handler {
+	var handler http.Handler
+	switch lbType {
+	case "round_robin":
+		logger.Info("Using Round Robin Load Balancer")
+		handler = round_robin.NewLoadBalancerHandler(logger, getBackendUrls())
+	case "least_connections":
+		logger.Info("Using Least Connections Load Balancer")
+		handler = least_connections.NewLoadBalancerHandler(logger, getBackendUrls())
+	}
+
+	chainedHandler := middleware.Chain(
+		handler,
+		middleware.NewMiddlewares(logger)...,
+	)
+	return chainedHandler
 }
 
 func getBackendUrls() []*url.URL {
