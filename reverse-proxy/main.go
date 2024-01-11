d65d5c7a5eb6bff5e23afdc7f2b668d33011a6cc
tasuku43 <task.teckac@gmail.com>
2024-01-11 09:42:16 +0900

    refactor: handlerを別関数に切り出した

--- reverse-proxy/main.go
+++ reverse-proxy/main.go
@@ -17,6 +17,27 @@ var (
 	loadBalancer = load_balancer.NewLoadBalancer(getBackendUrls())
 )
 
+func handler(logger *slog.Logger, l load_balancer.LoadBalancer) http.Handler {
+	proxy := &httputil.ReverseProxy{
+		Director: func(r *http.Request) {
+			backendUrl := l.Next()
+			logger.Info("Proxying request to: " + backendUrl.String())
+			r.URL.Scheme = backendUrl.Scheme
+			r.URL.Host = backendUrl.Host
+		},
+		Transport: &http.Transport{
+			MaxIdleConns:        10,
+			IdleConnTimeout:     90 * time.Second,
+			DisableCompression:  true,
+			MaxIdleConnsPerHost: 10,
+		},
+	}
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		proxy.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	var logger = slog.New(slog.NewJSONHandler(os.Stdout, nil))
 
@@ -25,13 +46,7 @@ func main() {
 		return
 	}
 
-	proxy := createProxy(logger, loadBalancer)
-
-	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		proxy.ServeHTTP(w, r)
-	})
-
-	chainedHandler := middleware.Chain(handler, middleware.NewMiddlewares(logger)...)
+	chainedHandler := middleware.Chain(handler(logger, loadBalancer), middleware.NewMiddlewares(logger)...)
 
 	http.Handle("/", chainedHandler)
 
@@ -40,27 +55,6 @@ func main() {
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		return
 	}
-}
-
-func createProxy(logger *slog.Logger, l load_balancer.LoadBalancer) *httputil.ReverseProxy {
-	customClient := &http.Client{
-		Transport: &http.Transport{
-			MaxIdleConns:        10,
-			IdleConnTimeout:     90 * time.Second,
-			DisableCompression:  true,
-			MaxIdleConnsPerHost: 10,
-		},
-	}
-	proxy := &httputil.ReverseProxy{
-		Director: func(r *http.Request) {
-			backendUrl := l.Next()
-			logger.Info("Proxying request to: " + backendUrl.String())
-			r.URL.Scheme = backendUrl.Scheme
-			r.URL.Host = backendUrl.Host
-		},
-		Transport: customClient.Transport,
-	}
-	return proxy
 }
 
 func getBackendUrls() []*url.URL {
